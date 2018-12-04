fc7a99bf7967178e05648fca24d9b4f4b86bd361
Matt Guptil <mdguptil@gmail.com>
2018-12-04 14:29:56 -0500

    Added library for icons. Added code to try and render an image library with tile bars describing the images. Currently it won't render because of an issue with tile.map in line 111.

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -2,12 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import leftSide from './leftSide' ;
-import {Button, Typography, Grid, Paper} from '@material-ui/core';
+import {ListSubheader, IconButton, GridList, GridListTile, GridListTileBar, Button, Typography, Grid, Paper} from '@material-ui/core';
 import axios from 'axios'
 import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import green from '@material-ui/core/colors/green';
+import { ThreeDRotationIcon } from '@material-ui/icons/ThreeDRotation';
+import tileData from './tileData';
 
 const theme = createMuiTheme({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
   palette: {
     primary: {
       main: '#43a047'
@@ -76,12 +92,40 @@ class App extends Component {
 
         <leftSideButtons />
         </Grid>
+
         <Grid xs={1}>
         </Grid>
+
         <Grid xs={3}>
         <Paper>
         Place Images Here <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
         </Paper>
+
+
+        <GridList cellHeight={180} cols={3}>
+
+        <GridListTile key="Subheader" cols={2} style={{height: 'auto'}}>
+          <ListSubheader component="div"> Library </ListSubheader>
+        </GridListTile>
+
+        {tileData.map(tile => (
+          <GridListTile key={tile.img}>
+          <img src={tile.img} alt={tile.tile} />
+          <GridListTileBar
+            title={tile.title}
+            subtitle={<span>by: {tile.author}</span>}
+            actionIcon={
+              <IconButton>
+                <ThreeDRotationIcon />
+              </IconButton>
+            }
+          />
+
+          </GridListTile>
+          ))}
+        </GridList>
+
+
         </Grid>
       </Grid>
 
