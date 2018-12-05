42ae9501ea9e1df9219064d1b4fd5716b75b39cf
Howard <howard.li@duke.edu>
2018-12-04 19:15:17 -0500

    HL: Adding checks for image validity and grayscale

--- processing.py
+++ processing.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as numpy
 from skimage import exposure
 from skimage import util
 from skimage import filters
@@ -13,57 +13,60 @@ class Processing(object):
     def __init__(self):
         pass
 
-    @staticmethod
-    def hist_eq(image):
+    def hist_eq(self, image):
         """
         Employs histogram equalization on given image.
         Args:
             image: Image to perform histogram equalization on.
         """
+        self._check_valid_image(image)
+        self._check_grayscale(image)  # Only works for grayscale images
         image_he = exposure.equalize_hist(image)
         return image_he
 
-    @staticmethod
-    def contrast_stretch(image, percentile=(10, 90)):
+    def contrast_stretch(self, image, percentile=(10, 90)):
         """
         Employs contrast stretching on given image.
         Args:
             image: Image to perform contrast stretching on.
             percentile: percentile range of pixel intensity to stretch
         """
-        p1, p2 = np.percentile(image, percentile)
+        self._check_valid_image(image)
+        p1, p2 = numpy.percentile(image, percentile)
         image_rescale = exposure.rescale_intensity(image, in_range=(p1, p2))
         return image_rescale
 
-    @staticmethod
-    def log_compression(image, base=10):
+    def log_compression(self, image, base=10):
         """
         Performs log compression of the image.
         Args:
             image: Image to perform inversion on.
             base: base of the log which is applied to the image
         """
-        image_log = np.log(image + 1) / np.log(base)
+        self._check_valid_image(image)
+        image_log = numpy.log(image + 1) / numpy.log(base)
         return image_log
 
-    @staticmethod
-    def reverse_video(image):
+    def reverse_video(self, image):
         """
         Creates a reverse video of given video (image/frame list).
+        Only works for grayscale images
         Args:
             image: Image to perform inversion on.
         """
+        self._check_valid_image(image)
+        self._check_grayscale(image)
         image_reverse = util.invert(image)
         return image_reverse
 
-    @staticmethod
-    def blur(image, sigma=5):
+    def blur(self, image, sigma=5):
         """
         Employs a blurring filter on given image.
         Args:
             image: Image to perform blurring on.
             sigma: Standard deviation for Gaussian blur kernel
         """
+        self._check_valid_image(image)
         image_blur = filters.gaussian(image, sigma)
         return image_blur
 
@@ -74,6 +77,7 @@ class Processing(object):
             image: Image to perform sharpening on.
             filter_type: The type of the filter to use.
         """
+        self._check_valid_image(image)
         # image_sharpened = filters.unsharp_mask(image, radius=1, amount=1)
         # unsharp_mask but it doesn't seem to exist in
         # skimage.filters anymore
@@ -92,4 +96,21 @@ class Processing(object):
             bool: If the image is valid.
         """
         # Image input should be an ARRAY.
+        if type(image) != numpy.ndarray:
+            raise TypeError("Image is not a numpy array")
+        # Image array should be grayscale or color (length = 2 or 3)
+        if len(image.shape) != 2 and len(image.shape) != 3:
+            raise TypeError("Dimensions of input array incorrect")
+        return True
+
+    def _check_grayscale(self, image):
+        """
+        Checks if the input image is grayscale.
+
+        Returns:
+            bool: True if the image is grayscale.
+        """
+        # Image array length should not be 3 (color).
+        if len(image.shape) == 3:
+            raise TypeError("Image is a color image")
         return True
