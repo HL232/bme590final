3eec2f8a2bb314d44ea1758c3d87f846c3b4aba7
Matt Guptil <mdguptil@gmail.com>
2018-12-05 20:54:15 -0500

    Added functions to the Library that automatically grab all the photos from the database/server when this component is opened. Also started to add functionality so that images and information will be autofilled into the GridList.

--- react-ui/src/Library.js
+++ react-ui/src/Library.js
@@ -1,6 +1,6 @@
  import React, { Component } from 'react';
 import {ListSubheader, IconButton, GridList, GridListTile, GridListTileBar, Button, Typography, Grid, Paper} from '@material-ui/core';
-
+import axios from 'axios'
 import image from './a.jpeg'
 const TileData = [
   {
@@ -12,24 +12,42 @@ const TileData = [
 export default class Library extends Component {
 
 state = {
-    myData: null
+    myData: []
+    
   }
 
 getData = () => {
-
+  console.log('getData')
+  axios.get("http://adpl.suyash.io/api/sites").then(res => {
+    console.log(res)
+    this.setState({myData: res.data})
+  })
+}
+  
+containData = () => {
+  if (this.state.myData.length === 0){
+    {this.getData()}
+  }
 }
 
-  render() {
 
+
+  render() {
+    
     return(
+      <div>
+      {this.containData()}
+
       <Paper backgroundcolor= "primary">
       <GridList cellHeight={500} cols={3}>
 
         <GridListTile key="Subheader" cols={1} style={{height: 'auto'}}>
           <ListSubheader component="div"> Library </ListSubheader>
         </GridListTile>
-
-        {TileData.map(tile => (
+        // All I have to do here is change this to myData.map and make sure
+        // that the '.' parts match the data type
+        // probably need a little tweaking to pull the thing out of the state though
+        {this.state.myData.map(tile => (
           <GridListTile key={tile.img}>
           <img src={tile.img} alt={tile.tile} />
           <GridListTileBar
@@ -47,6 +65,7 @@ getData = () => {
        
         </GridList>
         </Paper>
+        </div>
     )
   }
 }
