edb09cc077426bdf4c725f0f8402d832cc9f6b09
Howard <howard.li@duke.edu>
2018-12-07 21:44:53 -0500

    HL: Adding tests after Stephen's merge

--- processing.py
+++ processing.py
@@ -2,6 +2,9 @@ import datetime
 import skimage
 import numpy as np
 from skimage import exposure, util, filters
+import custom_errors
+import matplotlib.pyplot as plt
+from skimage.io import imread
 
 
 class Benchmark(object):
@@ -28,6 +31,8 @@ class Processing(object):
         Employs histogram equalization on given image.
         Args:
             image: Image to perform histogram equalization on.
+        Returns:
+            Numpy.Array representation of histogram equilization image
         """
         # TODO: Make sure you raise exceptions if not grayscale/
         # convert to grayscale for them.
@@ -41,6 +46,8 @@ class Processing(object):
         Args:
             image: Image to perform contrast stretching on.
             percentile: percentile range of pixel intensity to stretch
+        Returns:
+            Numpy.Array representation of contrast stretched image
         """
         b = Benchmark()
         p1, p2 = np.percentile(self.image, percentile)
@@ -51,7 +58,10 @@ class Processing(object):
         """
         Performs log compression of the image.
         Args:
+            image: Image to perform inversion on.
             base: base of the log which is applied to the image
+        Returns:
+            Numpy.Array representation of log compressed image
         """
         b = Benchmark()
         if len(self.image.shape) == 3 and self.image.shape[2] != 3:
@@ -66,8 +76,11 @@ class Processing(object):
     def reverse_video(self):
         """
         Creates a reverse video of given video (image/frame list).
+        Only works for grayscale images
         Args:
             image: Image to perform inversion on.
+        Returns:
+            Numpy.Array representation of reversed image
         """
         # TODO: Please check inputs here!
 
@@ -81,6 +94,8 @@ class Processing(object):
         Args:
             image: Image to perform blurring on.
             sigma: Standard deviation for Gaussian blur kernel
+        Returns:
+            Numpy.Array representation of blurred image
         """
         b = Benchmark()
         image_blur = filters.gaussian(self.image, sigma,
@@ -93,6 +108,8 @@ class Processing(object):
         Args:
             image: Image to perform sharpening on.
             filter_type: The type of the filter to use.
+        Returns:
+            Numpy.Array representation of sharpened image
         """
         # image_sharpened = filters.unsharp_mask(image, radius=1, amount=1)
         # unsharp_mask but it doesn't seem to exist in
@@ -104,3 +121,50 @@ class Processing(object):
         alpha = 1
         image_sharpened = self.image + alpha * (self.image - image_blur)
         return image_sharpened, b.stop()
+
+    def histogram_gray(self):
+        """
+        Returns a histogram of the image
+        Args:
+            image: Image to find histogram of
+        Returns:
+            Numpy.Array representation of histogram of image
+        """
+        plt.hist(self.image.ravel(), bins=256, range=(0.0, 1.0), color='black')
+        plt.xlabel('Normalized Pixel Intensity')
+        plt.ylabel('Number of Pixels')
+        plt.xlim(0, 1)
+        plt.savefig("./temp.png")
+        plt.close()
+
+        # this is a very crude method returning a numpy array
+        temp = imread('temp.png')
+        return temp
+
+    def _check_image_type(self):
+        """
+        Checks if the input image is valid to be processed.
+        Returns:
+            bool: If the image is valid.
+        """
+        # Image input should be an ARRAY.
+        if type(self.image) != np.ndarray:
+            raise TypeError("Image is not a numpy array")
+        return True
+
+    def _check_image_shape(self):
+        # Image array should be grayscale or color (length = 2 or 3)
+        if len(self.image.shape) != 2 and len(self.image.shape) != 3:
+            raise ValueError("Dimensions of input array incorrect")
+        return True
+
+    def _check_grayscale(self):
+        """
+        Checks if the input image is grayscale.
+        Returns:
+            bool: True if the image is grayscale.
+        """
+        # Image array length should not be 3 (color).
+        if len(self.image.shape) == 3:
+            raise custom_errors.GrayscaleError("Image is a color image")
+        return True
