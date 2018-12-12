7b3d8c8377aa612b6c42b05044963a80e0695ee7
Stephen <szx2@duke.com>
2018-12-12 01:14:45 -0500

    Fixes upload image problem on images with header.

--- random_thing.py
+++ random_thing.py
@@ -4,11 +4,7 @@ import json
 import base64
 import imageio
 import requests
-from random import choice
-from string import ascii_uppercase
 from matplotlib import pyplot as plt
-
-ip = "http://127.0.0.1:5000"
 
 
 def byte_2_json(resp):
@@ -46,25 +42,6 @@ def error_catcher(json_resp: dict):
     return json_resp
 
 
-def numpy_to_b64str(img, format="JPG"):
-    """
-    Converts a numpy array into a base 64 string
-    Args:
-        img (np.array):
-
-    Returns:
-        str: base 64 representation of the numpy array/image.
-
-    """
-    if _should_reverse_image(format):
-        # flip for cv conversion, only some file formats
-        img = img[..., ::-1]
-    _, img = cv2.imencode('.jpg', img)  # strips header
-    image_base64 = base64.b64encode(img)
-    base64_string = image_base64.decode('utf-8')  # convert to string
-    return base64_string
-
-
 def _should_reverse_image(format):
     should_reverse = ["JPG"]
     if format in should_reverse:
@@ -83,7 +60,7 @@ def b64str_to_numpy(b64_img):
         np.ndarray: numpy array of image.
 
     """
-    b64_image, _ = _get_b64_format(b64_img)
+    b64_img, _ = _get_b64_format(b64_img)
     byte_image = base64.b64decode(b64_img)
     image_buf = io.BytesIO(byte_image)
     np_img = imageio.imread(image_buf, format="JPG")
@@ -92,13 +69,32 @@ def b64str_to_numpy(b64_img):
 
 def _get_b64_format(b64_img):
     split = b64_img.split("base64,")  # get rid of header
-    image_format = None
     if len(split) == 2:
         b64_img = split[1]
         image_format = _determine_format(split[0])
     else:
         b64_img = split[0]
+        image_format = "JPG"  # assume jpg
     return b64_img, image_format
+
+
+def numpy_to_b64str(img, format="JPG"):
+    """
+    Converts a numpy array into a base 64 string
+    Args:
+        img (np.array):
+
+    Returns:
+        str: base 64 representation of the numpy array/image.
+
+    """
+    if _should_reverse_image(format):
+        # flip for cv conversion, only some file formats
+        img = img[..., ::-1]
+    _, img = cv2.imencode('.jpg', img)  # strips header
+    image_base64 = base64.b64encode(img)
+    base64_string = image_base64.decode('utf-8')  # convert to string
+    return base64_string
 
 
 def _determine_format(format_string: str):
@@ -128,48 +124,17 @@ def view_image(image):
     plt.show()
 
 
-email = "dukebme590.imageprocessor@gmail.com"
-dog_source = 'https://i.imgur.com/B15ubOP.jpg'
-# dog_source = "https://i.imgur.com/2gX8HVS.png"
-# dog_source = "MARBIBM.TIF"
-dog_image = imageio.imread(dog_source)
-# print("Original", dog_image.shape, dog_image[0][0])
+with open('b64testfile.txt') as f:
+    data = json.load(f)
+    image_b64 = data["image_data"]
 
-
-image_format = _determine_format(dog_source)
 image_obj = {
-    "email": email,
-    "image_data": numpy_to_b64str(dog_image, format=image_format),
-    "filename": dog_source
+    "email": "szx2@duke.edu",
+    "image_data": image_b64,
+    "filename": "test.jpg"
 }
 
 resp = requests.post("http://127.0.0.1:5000/api/process/upload_image",
                      json=image_obj)
 content = byte_2_json(resp)
-# view_image(b64str_to_numpy(content[0]["image_data"]))
-
-# blur
-image_obj_2 = {"email": email}
-resp = requests.post("http://127.0.0.1:5000/api/process/blur",
-                     json=image_obj)
-content = byte_2_json(resp)
-# view_image(b64str_to_numpy(content["image_data"]))
-
-# attempt to confirm
-resp = requests.post("http://127.0.0.1:5000/api/process/confirm", json=content)
-content = byte_2_json(resp)
-view_image(b64str_to_numpy(content["image_data"]))
-
-# should use the blurred image
-image_obj_3 = {"email": email}
-resp = requests.post("http://127.0.0.1:5000/api/process/sharpen",
-                     json=image_obj)
-content = byte_2_json(resp)
-view_image(b64str_to_numpy(content["image_data"]))
-
-# should use the non-sharpened blurred image, since not confirmed.
-image_obj_5 = {"email": email}
-resp = requests.post("http://127.0.0.1:5000/api/process/contrast_stretch",
-                     json=image_obj)
-content = byte_2_json(resp)
-view_image(b64str_to_numpy(content["image_data"]))
+view_image(b64str_to_numpy(content[0]["image_data"]))
