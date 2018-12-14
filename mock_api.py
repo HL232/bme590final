f7d2b9353bedc2828d108f4b4b72e43294b096d9
Howard <howard.li@duke.edu>
2018-12-14 12:52:57 -0500

    HL: howard's changes to mock_api

--- mock_api.py
+++ mock_api.py
@@ -1,11 +1,13 @@
+import imageio
 import requests
 from helper import *
 from img_processor_web_server import *
+from matplotlib import pyplot as plt
 
 domain = "vcm-7308.vm.duke.edu:5000"
 
-## Input your email or username
-email = ""
+# Input your email or username
+email = "blah@blah.com"
 
 # reads some images
 id_list = []  # Used for downloading
@@ -15,7 +17,7 @@ dog_image = imageio.imread(dog_source)
 dog_image_2 = imageio.imread(dog_source)
 image_format = determine_format(dog_source)
 
-## Uploading images
+# Uploading images
 # This is an example of uploading a single image
 # to the webserver. It returns a json of the
 # database image and sets it as current
@@ -70,9 +72,8 @@ view_image(b64str_to_numpy(content["image_data"]))
 # get_updated_images or get_original_images
 # endpoints
 resp = requests.get(
-    "http://{}/api/process/get_original_uploads".format(domain))
-content = byte_2_json(resp)
-print("List of image IDS: ", content)
+    "http://{}/api/user/get_original_uploads/{}".format(domain, email))
+# But this is too big to display in a juypter notebook
 
 # The web server internally stores the user's current
 # image and has api endpoints that allow them to scroll
@@ -91,25 +92,27 @@ resp = requests.post(
 content = byte_2_json(resp)
 print("Current Image ID (second ID): ",
       content["image_id"])
-# -------------
+
 # we can see that we do indeed go backwards,
 # in this case. This creates a tree-like structure
 # for the image history.
 resp = requests.get(
-    "http://{}/api/process/previous".format(domain))
+    "http://{}/api/image/get_previous_image/".format(domain, email))
 content = byte_2_json(resp)
 print("New Current Image ID (first ID): ",
       content["image_id"])
 
 content = byte_2_json(resp)
 
-## Processing images
+# Processing images
 # upload initial image
 resp = requests.post(
     "http://{}/api/process/upload_image".format(domain),
-    json=image_obj_2)
+    json=image_obj)
 content = byte_2_json(resp)
 view_image(b64str_to_numpy(content["image_data"]))
+
+
 # In order to process an image, the server performs
 # actions on the current image, and acts upon it without
 # storing it into the database/gives it to the GUI,
@@ -120,77 +123,73 @@ resp = requests.post(
     "http://{}/api/process/sharpen".format(domain),
     json=image_obj_2)
 content = byte_2_json(resp)
+print('Original Image:')
 view_image(b64str_to_numpy(content["image_data"]))
 # blurring
 resp = requests.post(
     "http://{}/api/process/blur".format(domain),
     json=image_obj_2)
 content = byte_2_json(resp)
+print('Blur')
 view_image(b64str_to_numpy(content["image_data"]))
 # log compression
 resp = requests.post(
     "http://{}/api/process/log_compression".format(domain),
     json=image_obj_2)
 content = byte_2_json(resp)
+print('Log Compression')
 view_image(b64str_to_numpy(content["image_data"]))
 # contrast stretching
 resp = requests.post(
     "http://{}/api/process/contrast_stretch".format(domain),
     json=image_obj_2)
 content = byte_2_json(resp)
+print('Contrast Stretch')
 view_image(b64str_to_numpy(content["image_data"]))
 # histogram equalization
 resp = requests.post(
     "http://{}/api/process/hist_eq".format(domain),
     json=image_obj_2)
 content = byte_2_json(resp)
+print('Histogram Equalization')
 view_image(b64str_to_numpy(content["image_data"]))
 
 # reverse video
 # NOTE it only works for grayscale images
+# Here is a grayscale image to test.
+resp = requests.post(
+    "http://{}/api/process/upload_image".format(domain),
+    json=image_obj_2)
+content = byte_2_json(resp)
+print('Original Gray Image:')
+view_image(b64str_to_numpy(content["image_data"]))
 resp = requests.post(
     "http://{}/api/process/reverse_video".format(domain),
     json=image_obj_2)
 content = byte_2_json(resp)
-print(content)
-# Here is a grayscale image to test.
-resp = requests.post(
-    "http://{}/api/process/upload_image".format(domain),
-    json=image_obj)
-content = byte_2_json(resp)
-view_image(b64str_to_numpy(content["image_data"]))
-resp = requests.post(
-    "http://{}/api/process/reverse_video".format(domain),
-    json=image_obj)
-content = byte_2_json(resp)
+print('Reverse Video')
 view_image(b64str_to_numpy(content["image_data"]))
 
-## Confirming
+
+# Confirming
 # Notice that none of the actions done to the initial
 # Image was permanent. In order to commit a to a change
 # or a processing functionality, you need to confirm
-# This will confirm the reverse video gray dog from
-# above, and then subsequently blur that reversed image.
 confirmed = requests.post(
     "http://{}/api/process/confirm".format(domain),
     json={"email": email})
 confirmed = byte_2_json(resp)
-resp = requests.post(
-    "http://{}/api/process/blur".format(domain),
-    json=image_obj_2)
-content = byte_2_json(resp)
-id_list.append(content["image_id"])
-view_image(b64str_to_numpy(content["image_data"]))
+print(confirmed)
 
-## Process count
+# Process count
 # Every time the user does a process, it is logged.
 # The count will increase, just as a record.
 resp = requests.get(
-    "http://{}/user/get_user/{}".format(domain, email))
+    "http://{}/api/user/get_user/{}".format(domain, email))
 content = byte_2_json(resp)
 print(content["process_count"])
 
-## Metadata
+# Metadata
 # Additionally, note that every single image has a
 # significant amount of metadata associated with it.
 # These include: parent-child relationships with other
@@ -198,11 +197,11 @@ print(content["process_count"])
 # image size, and the histogram
 confirmed_2 = confirmed
 del confirmed_2["image_data"]
-histogram = confirmed_2["histogram"]
+del confirmed_2["histogram"]
 print(confirmed_2)
-view_image(b64str_to_numpy(histogram))
 
-## Downloading
+# DOESNT WORK*******************************************************
+# Downloading
 # Downloading comes in several different forms: single
 # or zipped in a file with a file type or your choice
 # attempt to get zipped images
@@ -223,7 +222,7 @@ content = byte_2_json(resp)
 np_image = b64str_to_numpy(content[0]["image_data"])
 imageio.imwrite(image_name)
 
-# --------------
+# DOESN'T WORK******************************************************
 # zip file and specific formats
 zip_post = {
     "image_ids": id_list,
